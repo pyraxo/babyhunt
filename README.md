032f70eb9190d6b6cebe6c766dd1e90501463f82
pyraxo <pyraxo@pyraxo.moe>
2023-03-14 04:28:26 +0800

    fix deployment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Baby Hunt
 
-Companion Telegram bot for the SUTD easter egg hunt - **Baby Hunt**
+[Companion Telegram bot](https://t.me/babyhuntbot) for the SUTD easter egg hunt - **Baby Hunt**
 
 ## Deployment
 
@@ -18,3 +18,9 @@ npm run deploy
 POST `https://api.telegram.org/bot<botToken>/setWebhook?url=<functionURL>`
 
 GET `https://api.telegram.org/bot<botToken>/getwebhookInfo`
+
+## Additional Notes
+
+This project is purely for entertainment purposes only. I wrote this to learn about Firebase Functions and Telegraf, and completed it in a day.
+
+While you can very easily reverse-engineer the baby code generation, this goes against the spirit of the baby hunt.
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -29,6 +29,18 @@ interface User {
   count: number;
 }
 
+const encrypt = (text: string) => {
+  let val = Buffer.from(text).toString("hex");
+  val = val + Array.from(val).reverse().join("");
+  return Buffer.from(val).toString("base64");
+};
+
+const decrypt = (text: string) => {
+  let val = Buffer.from(text, "base64").toString();
+  val = val.substring(0, val.length / 2);
+  return Buffer.from(val, "hex").toString();
+};
+
 bot.start(async (ctx) => {
   if (!ctx.startPayload) {
     return ctx.reply(
@@ -40,7 +52,7 @@ bot.start(async (ctx) => {
       ].join("\n")
     );
   }
-  const url = Buffer.from(ctx.startPayload, "base64").toString();
+  const url = decrypt(ctx.startPayload);
   const params = Object.fromEntries(new URLSearchParams(url).entries());
 
   const { babyId } = params;
@@ -51,7 +63,9 @@ bot.start(async (ctx) => {
   const babyRef = db.collection("babies").doc(babyId);
 
   try {
-    return db.runTransaction(async (t) => {
+    let newBaby = false;
+    const finder = ctx.message.from.username;
+    await db.runTransaction(async (t) => {
       const doc = await t.get(babyRef);
       if (!doc.exists) {
         return ctx.reply(
@@ -74,7 +88,26 @@ bot.start(async (ctx) => {
           timestamp: +Date.now(),
           username: finder,
         });
-        const userRef = db.collection("users").doc(finder);
+        newBaby = true;
+        return ctx.reply(
+          [
+            `👶 You found Baby \\#*${babyId}*\\!\n`,
+            "Congratulations\\! Hope you find more babies\\!",
+          ].join("\n"),
+          { parse_mode: "MarkdownV2" }
+        );
+      }
+      return ctx.reply(
+        [
+          `😢 Oops\\! Baby \\#*${babyId}* has already been found\\!\n`,
+          "Continue hunting more babies\\!",
+        ].join("\n"),
+        { parse_mode: "MarkdownV2" }
+      );
+    });
+    if (newBaby) {
+      const userRef = db.collection("users").doc(finder as string);
+      return await db.runTransaction(async (t) => {
         const userDoc = await t.get(userRef);
         if (!userDoc.exists) {
           t.set(userRef, { username: finder, count: 1 });
@@ -84,20 +117,8 @@ bot.start(async (ctx) => {
             count: userDoc.data()?.count + 1,
           });
         }
-        return ctx.reply(
-          [
-            `👶 You found Baby #**${babyId}**!\n`,
-            "Congratulations! Hope you find more babies!",
-          ].join("\n")
-        );
-      }
-      return ctx.reply(
-        [
-          `😢 Oops! Baby #**${babyId}** has already been found!\n`,
-          "Continue hunting more babies!",
-        ].join("\n")
-      );
-    });
+      });
+    }
   } catch (err) {
     functions.logger.error(`[BabyHunt] Error updating baby ${babyId}`, err);
     return ctx.reply("Oops! Something went wrong. Contact @aarontzy");
@@ -111,19 +132,19 @@ bot.command("add", async (ctx) => {
     return db.runTransaction(async (t) => {
       const doc = await t.get(countRef);
       if (!doc.exists) {
-        t.update(countRef, { count: 0 });
+        t.set(countRef, { count: 0 });
         functions.logger.info("[BabyHunt] Initialising new info data");
         return;
       }
       const { count } = doc.data() as Info;
       const newCount = count + 1;
-      t.update(db.collection("babies").doc(`${newCount}`), {
+      await db.collection("babies").doc(`${newCount}`).set({
         timestamp: 0,
         username: "",
       });
       t.update(countRef, { count: newCount });
       functions.logger.info(`[BabyHunt] Adding new baby ${newCount}`);
-      const b64 = Buffer.from(`babyId=${newCount}`).toString("base64");
+      const b64 = encrypt(`babyId=${newCount}`);
       return ctx.reply(
         `Adding new baby ${newCount}: https://t.me/babyhuntbot?start=${b64}`
       );
@@ -137,7 +158,7 @@ bot.command("add", async (ctx) => {
 bot.command("leaderboard", async (ctx) => {
   const userRef = db.collection("users");
   const topFive = await userRef.orderBy("count", "desc").limit(5).get();
-  const resp = ["👶 **Baby Hunter Leaderboards**:\n"];
+  const resp = ["👶 *Baby Hunter Leaderboards*:\n"];
   let num = 0;
   topFive.forEach((doc) => {
     const { username, count } = doc.data() as User;
@@ -148,7 +169,7 @@ bot.command("leaderboard", async (ctx) => {
       "😪 Nobody's found a baby yet! Maybe you can be the first one?"
     );
   }
-  return ctx.reply(resp.join("\n"));
+  return ctx.reply(resp.join("\n"), { parse_mode: "MarkdownV2" });
 });
 
 exports.babyHuntBot = functions.https.onRequest(async (request, response) => {
